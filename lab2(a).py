936a868ffd28153e698ffbc99850d235bf75d525
bhavyhegde1188 <139729959+bhavyhegde1188@users.noreply.github.com>
2023-09-02 23:24:21 +0530

    Create lab2(a).py

---
+++ lab2(a).py
@@ -0,0 +1,10 @@
+print("Fibonacci Sequence")
+while (count < n):
+print(a, end = " ")
+c = a + b
+a = b
+b = c
+count += 1
+Enter the value of n: 15
+Fibonacci Sequence
+0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
