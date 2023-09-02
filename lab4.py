f746f2a3706a6f1d7f766b8da0300d274f412cbe
bhavyhegde1188 <139729959+bhavyhegde1188@users.noreply.github.com>
2023-09-02 23:36:38 +0530

    Create lab4.py

---
+++ lab4.py
@@ -0,0 +1,44 @@
+num = input("Enter a multi digit number: ")
+zeros = ones = twos = three = four = five = six = seven = eight = nine = 0
+for i in num:
+if i == '0':
+zeros = zeros + 1
+if i == '1':
+ones = ones + 1
+if i == '2':
+twos = twos + 1
+if i == '3':
+three = three + 1
+if i == '3':
+four = four + 1
+if i == '3':
+five = five + 1
+if i == '3':
+six = six + 1
+if i == '3':
+seven = seven + 1
+if i == '3':
+eight = eight + 1
+if i == '3':
+nine = nine + 1
+print("Zero's = ", zeros)
+print("One's = ", ones)
+print("Two's = ", twos)
+print("Three's = ", three)
+rint( Four s = , four)
+print("Five's = ", five)
+print("Six's = ", six)
+print("Seven's = ", seven)
+print("Eight's = ", eight)
+print("Nine's = ", nine)
+Enter a multi digit number: 545646123
+Zero's = 0
+One's = 1
+Two's = 1
+Three's = 1
+Four's = 1
+Five's = 1
+Six's = 1
+Seven's = 1
+Eight's = 1
+Nine's = 1
