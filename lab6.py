83df6056bcee63f569daab2d4ae882e267ce879b
bhavyhegde1188 <139729959+bhavyhegde1188@users.noreply.github.com>
2023-09-03 00:39:59 +0530

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,12 +1,10 @@
 #Develop a program to sort the text file and write the sorted content into a separate file
 ifile = open('text.txt')  
-   ofile = open('otext.txt', mode = 'w')  
-  sentence = ifile.read()  
-  
- words sentence.split()  
- words.sort()  
- print (words)  
-  
- for word in words:  
+ofile = open('otext.txt', mode = 'w')  
+sentence = ifile.read()  
+words sentence.split()  
+words.sort()  
+print (words)  
+for word in words:  
   ofile.write(word + " ")  
- ofile.close()
+ofile.close()
