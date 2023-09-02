9be6200375863f9af9d7bb95e8ee9d75c0bcc2a9
bhavyhegde1188 <139729959+bhavyhegde1188@users.noreply.github.com>
2023-09-02 23:07:52 +0530

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,15 @@
+name = input("Enter the student name: ")
+usn = input("Enter the student USN: ")
+phy = int(input("Enter the marks obtained in Physics: "))
+che = int(input("Enter the marks obtained in Chemistry: "))
+mat = int(input("Enter the marks obtained in Mathematics: "))
+max_marks = 300
+print(30 * "*")
+print("Name of the Student:", name)
+print("Student USN:", usn)
+print(30 * "*")
+total = phy + che + mat
+print("The total marks scored by the student in Physics", phy, "Chemistry", che, "Mathematics", mat, "is", total)
+print(30 * "*")
+percent = (total/max_marks) * 100
+print("Percentage of the Student:", round(percent))
